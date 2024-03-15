a67bc52d1ae91278fed3e4519de9481ac4068b07
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-15 22:25:38 +0530

    fix: template added

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import sayHello
+from . import views
 
 
 urlpatterns = [
-    path("", sayHello)
+    path('', views.index, name='index')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def sayHello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def index(request):
+    return render(request, 'index.html', {})
