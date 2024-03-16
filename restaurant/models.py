22da91e0173168b8680671a15b2438e3a8b929b6
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-16 19:56:06 +0530

    fix: added booking and menu models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.TextField(null=True)
+    no_of_guests = models.IntegerField(null=True)
+    bookingDate = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.TextField(null=True)
+    price = models.DecimalField(null=True, max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.title
