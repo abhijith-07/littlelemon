bd64c735e7d8011eda40fb3fb314d2255cc87d15
Abhijith Subash <abhijithsubash07@gmail.com>
2024-03-18 18:34:16 +0530

    fix: tested api with postman

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "reservation",
     "restaurant",
     "rest_framework",
+    "rest_framework.authtoken",
 
     "django.contrib.admin",
     "django.contrib.auth",
@@ -120,3 +121,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+   'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+   ),
+   'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser'
+   ),
+}
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import BookingSerializer, MenuSerializer
 from .models import Booking, Menu
 
 
-def index(request):
-    return render(request, 'index.html', {})
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
